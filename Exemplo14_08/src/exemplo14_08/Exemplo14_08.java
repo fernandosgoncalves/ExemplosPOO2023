c366f801d79a0811c9c1f997d74bfcb8d769c3e8
professor <professor>
2023-08-14 20:01:24 -0300

    Primeiro Projeto

---
+++ Exemplo14_08/src/exemplo14_08/Exemplo14_08.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo14_08;
+
+/**
+ *
+ * @author professor
+ */
+public class Exemplo14_08 {
+    
+    int x;
+    char a;
+    String texto;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Soma de 5 + 3 = " 
+                + soma(15,4));
+    }
+    
+    public static int soma(int a, int b){
+        return a+b;
+    }
+}
