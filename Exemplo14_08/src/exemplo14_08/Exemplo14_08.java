a7f9d77d15482009975eb28da0e28f5f0043b27b
professor <professor>
2023-08-14 21:21:44 -0300

    Exemplos aula

--- Exemplo14_08/src/exemplo14_08/Exemplo14_08.java
+++ Exemplo14_08/src/exemplo14_08/Exemplo14_08.java
@@ -5,6 +5,8 @@
  */
 package exemplo14_08;
 
+import java.util.Scanner;
+
 /**
  *
  * @author professor
@@ -18,8 +20,39 @@ public class Exemplo14_08 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Scanner lerString = new Scanner(System.in);
+        
+        String teste;
+        
+        teste = lerString.nextLine();
+        
+        System.out.println(teste);
+        
+        int x, y;
+        
+        System.out.println("Informe o primeiro numero: ");
+        x = ler.nextInt();
+        
+        System.out.println("Informe um segundo numero: ");
+        y = ler.nextInt();
+        
+        System.out.println("Soma de " + x + " + "
+                + y + " = " + soma(x,y)); 
+                        
         System.out.println("Soma de 5 + 3 = " 
-                + soma(15,4));
+                + soma(5,3));
+
+        float a = (float) 4.5;
+        float b, c;
+        b = 3;
+        c = (float) 8.9;
+        
+        System.out.println("A + B = " + (a+b));
+        System.out.println("B * C = " + b*c);
+        
+        System.out.println("A + B = " + (a+b) + "\n"
+                + "B * C = " + b*c);
     }
     
     public static int soma(int a, int b){
